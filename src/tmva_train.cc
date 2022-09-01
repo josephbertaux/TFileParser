22afc5695d6fb747cae414cc6932695d62ffff08
josephbertaux <jbertau@purdue.edu>
2022-08-31 21:50:07 -0400

    Sample Text

--- src/tmva_train.cc
+++ src/tmva_train.cc
@@ -2,18 +2,138 @@
 
 void TMVA_Trainer::MakeTrees()
 {
+	uint u;
+
+	RooArgList training_args;
+	std::vector<double> training_vals = {};
+	for(u = 0; u < training_branches.size(); u++)
+	{
+		training_vals.push_back(0.0);
+		training_args.add(*(new RooRealVar(training_branches[u].c_str(), training_branches[u].c_str(), 0.0, -DBL_MAX, DBL_MAX)));
+	}
+
+	size_t pos = std::string::npos;
+	std::string temp1 = "";
+	std::string temp2 = "";
+
+	RooArgList expression_args;
+	std::vector<double> expression_vals = {};
+	for(u = 0; u < training_expressions.size(); u++)
+	{
+		pos = training_expressions[u].find("=");
+
+		if(pos == std::string::npos)
+		{
+			std::cout << "Bad expression, does not contain '='" << std::endl;
+			continue;
+		}
+
+		temp1 = training_expressions[u].substr(0, pos);
+		temp2 = training_expressions[u].substr(pos + 1);
+
+		expression_vals.push_back(0.0);
+		expression_args.add(*(new RooFormulaVar(temp1.c_str(), temp2.c_str(), training_args)));
+	}
+
+
+
+	std::string file_name = "";
+	TFile* current_file = 0x0;
+	TTree* current_tree = 0x0;
+	Long64_t n;
+
 	if(!signal_tree)
 	{
-		training_branch_values.clear();
-
 		signal_tree = new TTree("signal_tree", "signal_tree");
-		for(uint u = 0; u < training_expressions.size(); u++)
+		for(u = 0; u < training_expressions.size(); u++)
 		{
-			training_branch_values.push_back(0.0);
+			pos = training_expressions[u].find("=");
 	
-			signal_tree->Branch(Form("expr%d", u));
+			if(pos == std::string::npos)continue;
+
+			temp1 = training_expressions[u].substr(0, pos);
+			signal_tree->Branch(temp1.c_str()); //probably edit later 
+		}
+		signal_tree->ResetBranchAddresses();
+	}
+
+	for(u = 0; u < expression_vals.size(); u++)
+	{
+		signal_tree->SetBranchStatus(training_expressions[u].c_str(), 1);
+		signal_tree->SetBranchAddress(training_expressions[u].c_str(), &(expression_vals[u]));
+	}
+
+	std::ifstream file_list(signal_list_file_name.c_str(), std::ios_base::in);
+	while(file_list.is_open())
+	{
+		if(file_list.bad())break;
+
+		file_list >> file_name;
+
+		if(file_list.eof())break;
+
+		current_file = TFile::Open(file_name.c_str(), "r");
+		if(!current_file)
+		{
+			std::cout << "Could not open file:" << std::endl;
+			std::cout << file_name << std::endl;
+			std::cout << "continuing" << std::endl;
+		}
+
+		current_tree = (TTree*)current_file->Get(training_tree_name.c_str());
+		if(!current_tree)
+		{
+                        std::cout << "Could not get tree:" << std::endl;
+			std::cout << training_tree_name << std::endl;
+			std::cout << "From file:" << std::endl;
+                        std::cout << file_name << std::endl;
+                        std::cout << "continuing" << std::endl;
+		}
+
+		current_tree->SetBranchStatus("*", 0);
+		for(u = 0; u < training_vals.size(); u++)
+		{
+			current_tree->SetBranchStatus(training_branches[u].c_str(), 1);
+			current_tree->SetBranchAddress(training_branches[u].c_str(), &(training_vals[u]));
+		}
+
+		for(n = 0; n < current_tree->GetEntriesFast(); n++)
+		{
+			current_tree->GetEntry(n);
+
+			for(u = 0; u < training_vals.size(); u++)
+			{
+				((RooRealVar*)&training_args[u])->setVal(training_vals[u]);
+			}
+
+			for(u = 0; u < expression_args.size(); u++)
+			{
+				expression_vals[u] = ((RooFormulaVar*)&expression_args[u])->getValV();
+			}
+
+			signal_tree->Fill();
 		}
 	}
 
-	
+	for(u = 0; u < training_args.getSize(); u++)
+	{
+		delete &training_args[u];
+	}
+
+	for(u = 0; u < expression_args.getSize(); u++)
+	{
+		delete &expression_args[u];
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/tmva_train.h
+++ src/tmva_train.h
@@ -1,8 +1,9 @@
-#ifndef TMVA_TRAIN_H
-#define TMVA_TRAIN_H
+#ifndef TMVA_TRAINER_H
+#define TMVA_TRAINER_H
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <string>
 #include <vector>
@@ -20,20 +21,23 @@
 #include "TMVA/Tools.h"
 #include "TMVA/TMVAGui.h"
 
+#include "RooRealVar.h"
+#include "RooFormulaVar.h"
+#include "RooArgList.h"
+
 class TMVA_Trainer
 {
 protected:
         TTree* signal_tree = 0x0;
         TTree* background_tree = 0x0;
 
-        std::vector<std::string> training_branches_names; //A vector containing the names of branches needed from the raw data
-	std::vector<double> training_branch_values;
-        std::vector<std::string> training_expressions; //A vector containing the expressions, in terms of branchnames, to train on
-
 public:
         std::string signal_list_file_name = ""; //The fullpath name of the .list file, listing files containing raw data for signal
         std::string background_list_file_name = ""; //The fullpath name of the .list file, listing files containing raw data for background
         std::string training_tree_name = ""; //The fullpath name of the TTree in each file of the filelist
+
+        std::vector<std::string> training_branches; //A vector containing the names of branches needed from the raw data
+        std::vector<std::string> training_expressions; //A vector containing the expressions, in terms of branchnames, to train on
 
         void MakeTrees();
 };

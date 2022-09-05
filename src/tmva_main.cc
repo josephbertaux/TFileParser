ed4187898652fe9cd4fb83cb2b7738ff7b6deb8c
josephbertaux <jbertau@purdue.edu>
2022-09-04 20:18:09 -0400

    Sample Text

--- src/tmva_main.cc
+++ src/tmva_main.cc
@@ -4,16 +4,16 @@ int main()
 {
 	TMVA_Trainer tmva_trainer;
 
-	std::cout << "hello" << std::endl;
-	
 	tmva_trainer.signal_list_file_name = "/home/jbertau/Data/BD0_Training/Dum/my_dum_signal_files.list";
 	tmva_trainer.training_tree_name = "tree";
+
+	//Be careful here--make sure you include all the original branches the expressions you want depend on
+	tmva_trainer.training_expressions.push_back("c=a+b");
+	tmva_trainer.training_expressions.push_back("d=a*b");
 
 	tmva_trainer.training_branches.push_back("a");
 	tmva_trainer.training_branches.push_back("b");
 
-	tmva_trainer.training_expressions.push_back("c=a+b");
-	tmva_trainer.training_expressions.push_back("d=a*b");
 
 	tmva_trainer.MakeTrees();
 	tmva_trainer.MiscDebug();
